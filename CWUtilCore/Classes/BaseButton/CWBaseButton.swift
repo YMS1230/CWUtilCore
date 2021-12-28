31e3dc68e3ea2d6aaa0266b3f80cbd96a7b2b4c2
cocoadogs <cocoadogs@163.com>
2021-12-28 20:52:11 +0800

    基类button测试

--- CWUtilCore/Classes/BaseButton/CWBaseButton.swift
+++ CWUtilCore/Classes/BaseButton/CWBaseButton.swift
@@ -9,12 +9,20 @@ import UIKit
 
 class CWBaseButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        self.buildUI()
+        super.layoutSubviews()
+    }
+    
+    func buildUI() {
+        titleLabel?.textColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+    }
 }
